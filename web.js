8eef032c95b17342fb775c887687695dc620965f
Trautsj <jstuart.robertson@gmail.com>
2013-07-15 00:17:18 +0000

    Changed web.js again

--- web.js
+++ web.js
@@ -6,8 +6,10 @@ var MsgBuf = fs.readFileSync('index.html');
 var Msg = MsgBuf.toString('utf-8',1,28);
 
 app.get('/', function(request, response) {
-  response.send(Msg);
-}
+    /* var fs = reqruie('fs');
+    var content = fs.readFileSync('index.html', 'utf-8'); */
+    response.send(Msg);
+});
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
