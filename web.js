2c33bd5c91274b471911df80c078beefc5b527ba
Trautsj <jstuart.robertson@gmail.com>
2013-07-15 00:19:41 +0000

    Changed web.js again

--- web.js
+++ web.js
@@ -2,13 +2,13 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
-var MsgBuf = fs.readFileSync('index.html');
-var Msg = MsgBuf.toString('utf-8',1,28);
+/* var MsgBuf = fs.readFileSync('index.html');
+var Msg = MsgBuf.toString('utf-8',1,28); */
 
 app.get('/', function(request, response) {
-    /* var fs = reqruie('fs');
-    var content = fs.readFileSync('index.html', 'utf-8'); */
-    response.send(Msg);
+    var fs = reqruie('fs');
+    var content = fs.readFileSync('index.html', 'utf-8');
+    /* response.send(Msg); */
 });
 
 var port = process.env.PORT || 5000;
